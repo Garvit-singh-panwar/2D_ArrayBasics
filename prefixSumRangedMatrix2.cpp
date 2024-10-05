39959ab05e4dddca62c4f598bafff51825400f5b
Garvit-singh-panwar <garvits893@gmail.com>
2024-10-05 18:25:26 +0530

    Add files via upload

---
+++ prefixSumRangedMatrix2.cpp
@@ -0,0 +1,87 @@
+// Given a matrix 'a' of dimension n x m and 2 cordinate (l1 ,r1) and (l2,r2) .
+// return the sum of the rectangle from (l1 , r1) to (l2,r2).
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int cordinateMatrixsum(vector <vector<int> > &matrix ,  int l1 , int r1 , int l2 , int r2)
+{
+int sum = 0;
+int n = matrix.size();
+int m = matrix[0].size();
+vector <vector <int> > prefix( n , vector<int> (m));
+for(int i = 0; i < n ; i++)
+{
+    for(int j = 0; j < m; j++)
+    {
+        sum = sum + matrix[i][j];
+        prefix[i][j] = sum ;
+    }
+    sum = 0;
+}
+
+for(int i = 0 ; i< m ; i++ )
+{
+   for(int j = 0; j < n ; j++)
+   {
+    sum += prefix[j][i];
+    prefix [j][i] = sum;
+   }
+   sum =0;
+}
+
+int topSum = 0, leftSum = 0, topLeftSum=0;
+
+if(l1 != 0){
+    topSum = prefix[l1-1][r2];
+}
+if(r1 != 0)
+{
+    leftSum = prefix[l2][r1-1];
+}
+if(l1 != 0 && r2 != 0)
+{
+    topLeftSum = prefix[l1-1][r1-1];
+}
+
+sum = prefix[l2][r2] - topSum - leftSum + topLeftSum;
+
+return sum;
+
+}
+
+int main()
+{
+
+
+int n,m;
+ 
+cout << "Enter no of rows in a matrix " << endl;
+cin >> n;
+cout << "Enter no of column in a matrix " << endl;
+cin >> m;
+
+
+vector < vector <int> > matrix(n , vector < int> (m)) ;
+cout << "Enter elements of a matrix  " << endl;
+for(int i = 0 ; i < n ; i++)
+{
+    for(int j =0 ; j < m ; j++)
+    {
+        cin >> matrix[i][j];
+    }
+}
+
+cout << "cordinates of L1 and L2 is  between 0 and " << n-1  << endl;
+cout << "cordinates of R1 and R2 is  between 0 and " << m-1  << endl << endl; 
+
+int L1 , L2 , R1 , R2;
+cout << "Enter cordinate L1 R1 " << endl;
+cin >> L1 >>R1 ;
+cout << "Enter cordinate L2 R2 " << endl;
+cin >> L2 >>R2 ;
+
+cout << endl ;
+cout << "Sum of elements between cordinate L1 R1 and L2 R2 : " << cordinateMatrixsum(matrix , L1 , R1 , L2 , R2)  << endl;
+    return 0;
+}
